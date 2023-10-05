3db945f59a600b871b46b4f540b19da5eea7969a
troydelacruz7 <troydelacruz24176@gmail.com>
2023-10-05 00:12:51 -0400

    Worked/finish basic nav

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// Hamburger Menu
+
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav__link');
+
+hamburger.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+})
